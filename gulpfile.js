7ecca3e808d825c6249e6d3891a02c9ffbf90bc0
Sandi Rosyandi <ndi_racy@rocketmail.com>
2016-11-06 18:11:03 +0700

    Add introduction section

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,20 @@
 var gulp = require('gulp');
 var browserify = require('gulp-browserify');
+var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('default', function(){
   gulp.src('src/js/app.js')
     .pipe(browserify())
-    .pipe(gulp.dest('js'))
+    .pipe(gulp.dest('js'));
+
+  gulp.src('src/sass/app.scss')
+    .pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('css'));
 });
 
-gulp.watch('src/js/*.js');
+gulp.watch('src/js/*.js', ['default']);
+gulp.watch('src/sass/*.scss', ['default']);
--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,15 @@
-require('bootstrap-css-only');
+var $ = require('jquery');
+window.jQuery = $;
+require('fullpage.js');
+require('typed.js');
+
+$(document).ready(function(){
+  $('#container').fullpage();
+
+  $("#iam").typed({
+    backDelay: 2000,
+    loop: true,
+    strings: ['web developer', 'father', 'silent reader'],
+    typeSpeed: 100
+  });
+});
